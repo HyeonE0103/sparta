eb3814a33843789fadf973ca53b1ba8f9903197f
HyeonE <mag30102000@gmail.com>
2023-04-07 01:10:49 +0900

    크롤링 기초

--- pythonprac/hello.py
+++ pythonprac/hello.py
@@ -1,11 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
-rjson = r.json()
+URL = "https://movie.daum.net/ranking/reservation"
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(URL, headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
 
-rows = rjson['RealtimeCityAir']['row']
+#mainContent > div > div.box_ranking > ol > li:nth-child(1) > div > div.thumb_cont > strong
+movies = soup.select("#mainContent > div > div.box_ranking > ol > li")
 
-for a in rows:
-	gu_name = a['MSRSTE_NM']
-	gu_mise = a['IDEX_MVL']
-	print(gu_name, gu_mise)
+for movie in movies:
+    a = movie.select_one('div > div.thumb_cont > strong').text
+    if a is not None:	# None이 아닐때만
+    	print(a)
