c1a98fe7fbb0a92474a26f2accb5c72f3408a379
HyeonE <mag30102000@gmail.com>
2023-04-14 00:20:43 +0900

    스파르타피디아

--- projects/03.pedia/app.py
+++ projects/03.pedia/app.py
@@ -16,11 +16,13 @@ def home():
 def movie_post():
 	url_receive = request.form['url_give']
 	comment_receive = request.form['comment_give']
+	star_receive = request.form['star_give']
 	
 	headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 	data = requests.get(url_receive,headers=headers)
 	
 	soup = BeautifulSoup(data.text, 'html.parser')
+
 	
 	ogtitle = soup.select_one('meta[property="og:title"]')['content']
 	ogdesc = soup.select_one('meta[property="og:description"]')['content']
@@ -30,6 +32,7 @@ def movie_post():
 		'desc':ogdesc,
 		'image':ogimage,
 		'comment':comment_receive,
+		'star': star_receive,
   }
 	db.movies.insert_one(doc)
 	
