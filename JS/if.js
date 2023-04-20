71a964cfec86fdb51a6ecd5becac53d2b3cdaf5a
HyeonE <mag30102000@gmail.com>
2023-04-21 00:13:36 +0900

    조건문

---
+++ JS/if.js
@@ -0,0 +1,10 @@
+let shoesPrice = 40000
+if (shoesPrice < 50000) { // 신발 가격이 50000원보다 작으므로 해당 코드가 실행됨
+	console.log('신발을 사겠습니다.')
+}
+
+const capPrice = 50000
+if (capPrice < 50000) {
+	console.log('모자를 사지 않겠습니다.') // 모자 가격이 50000원보다 작지 않으므로 해당 코드가 실행되지 않음
+}
+
