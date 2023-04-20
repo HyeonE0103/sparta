70196dd68f2a429a9e2086166381c3f4cdb434c2
HyeonE <mag30102000@gmail.com>
2023-04-20 23:50:11 +0900

    연산자

--- JS/operator.js
+++ JS/operator.js
@@ -38,3 +38,38 @@ totalPrice += pantsPrice; // totalPrice = totalPrice + pantsPrice 와 동일
 console.log(totalPrice);
 totalPrice -= shirtsPrice; // totalPrice = totalPrice - shirtsPrice 와 동일
 console.log(totalPrice);
+
+//비교연산자
+console.log(1 < 2); // 1이 2보다 작은가? true
+console.log(2 <= 2); // 2가 2보다 작거나 같은가? true
+console.log(1 > 2); // 1이 2보다 큰가? false
+console.log(1 >= 2); // 1이 2보다 크거나 같은가? false
+
+//논리연산자
+let isOnSale = true;
+let isDiscountItem = true;
+
+console.log(isOnSale && isDiscountItem); // true && true 이므로 true
+console.log(isOnSale || isDiscountItem); // true || true 이므로 true
+
+isOnSale = false;
+console.log(isOnSale && isDiscountItem); // false && true 이므로 false
+console.log(isOnSale || isDiscountItem); // false || true 이므로 true
+
+isDiscountItem = false;
+console.log(isOnSale && isDiscountItem); // false && false 이므로 false
+console.log(isOnSale || isDiscountItem); // false || false 이므로 false
+
+console.log(!isOnSale); // !false 이므로 true
+
+//일치연산자
+console.log(1 === 1); // true
+console.log(1 === 2); // false
+console.log("Javascript" === "Javascript"); // true
+console.log("Javascript" === "javascript"); // 대소문자나 띄워쓰기도 다 정확히 일치해야 해요. 따라서 false
+
+//Q
+let item = 5;
+let item2 = 15;
+let total = item + item2;
+console.log(`총 ${total * 0.8}원에 물건을 구입합니다`);
