e11621133ee4e5d3ed69c5f65c68c07d11b5c7d4
HyeonE <mag30102000@gmail.com>
2023-04-21 00:17:30 +0900

    조건문

--- JS/if.js
+++ JS/if.js
@@ -1,10 +1,35 @@
-let shoesPrice = 40000
-if (shoesPrice < 50000) { // 신발 가격이 50000원보다 작으므로 해당 코드가 실행됨
-	console.log('신발을 사겠습니다.')
+let shoesPrice = 40000;
+if (shoesPrice < 50000) {
+  // 신발 가격이 50000원보다 작으므로 해당 코드가 실행됨
+  console.log("신발을 사겠습니다.");
 }
 
-const capPrice = 50000
+const capPrice = 50000;
 if (capPrice < 50000) {
-	console.log('모자를 사지 않겠습니다.') // 모자 가격이 50000원보다 작지 않으므로 해당 코드가 실행되지 않음
+  console.log("모자를 사지 않겠습니다."); // 모자 가격이 50000원보다 작지 않으므로 해당 코드가 실행되지 않음
 }
 
+shoesPrice = 50000;
+if (shoesPrice < 40000) {
+  console.log("신발을 사겠습니다.");
+} else {
+  console.log("너무 비싸요. 신발을 사지 않겠습니다."); // 신발 가격이 40000원보다 작지 않으므로 해당 코드가 실행됨
+}
+
+if (shoesPrice < 40000) {
+  console.log("신발을 사겠습니다.");
+} else if (shoesPrice <= 50000) {
+  console.log("고민을 해볼게요..."); // 신발 가격이 50000원보다 작거나 같으므로 않으므로 해당 코드가 실행됨
+} else {
+  console.log("너무 비싸요. 신발을 사지 않겠습니다.");
+}
+
+//Q
+const distance = 3;
+if (distance < 2) {
+  console.log("걸어가자");
+} else if (2 <= distance && 5 > distance) {
+  console.log("택시를 타자");
+} else {
+  console.log("기차를 타자");
+}
