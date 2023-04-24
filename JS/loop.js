314b5939c2bca5238ee064e5cbe23d941fc54737
HyeonE <mag30102000@gmail.com>
2023-04-24 21:33:58 +0900

    반복문과 조건문 활용

--- JS/loop.js
+++ JS/loop.js
@@ -7,3 +7,11 @@ while (temperature < 25) {
 for (let temperature = 20; temperature < 25; temperature++) {
   console.log(`for: ${temperature}도 정도면 적당한 온도입니다.`);
 }
+
+for (let i = 1; i < 21; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i}은(는) 짝수입니다`);
+  } else {
+    console.log(`${i}은(는) 홀수입니다`);
+  }
+}
