80d1fd0680ea9e625f13d396069c1bbcf56de73f
HyeonE <mag30102000@gmail.com>
2023-04-26 00:09:11 +0900

    클래스와 객체

--- JS/class.js
+++ JS/class.js
@@ -12,3 +12,39 @@ console.log(notebook); // Notebook { name: 'Macbook', price: 2000000, company: '
 console.log(notebook.name); // MacBook
 console.log(notebook.price); // 2000000
 console.log(notebook.company); // Apple
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  printInfo() {
+    console.log(`상품명: ${this.name}, 가격: ${this.price}`);
+  }
+}
+const notebook2 = new Product("Apple Mackbook", 2000000);
+notebook2.printInfo();
+
+const computer = {
+  name: "Apple Mackbook",
+  price: 2000000,
+  printInfo: function () {
+    console.log(`상품명: ${this.name}, 가격: ${this.price}`);
+  },
+};
+computer.printInfo();
+
+class Cloth {
+  constructor(color, size, price) {
+    this.color = color;
+    this.size = size;
+    this.price = price;
+  }
+  printInfo() {
+    console.log(
+      `색깔: ${this.color}, 사이즈: ${this.size}, 가격: ${this.price}`
+    );
+  }
+}
+const coat = new Cloth("black", "M", 100000);
+coat.printInfo();
