69603c221ed47c0d858085f03eb02a88885d5406
HyeonE <mag30102000@gmail.com>
2023-04-27 21:54:19 +0900

    배열

---
+++ JS/array.js
@@ -0,0 +1,27 @@
+const arr = new Array(1, 2, 3, 4, 5);
+const arr2 = [1, 2, 3, 4, 5];
+console.log(arr);
+console.log(arr2);
+
+const rainbowColors = ["red", "orange", "yellow"];
+
+console.log(rainbowColors[0]); // o번 인덱스를 통해서 데이터에 접근. red
+console.log(rainbowColors[1]); // 1번 인덱스를 통해서 데이터에 접근. orage
+console.log(rainbowColors[2]); // 2번 인덱스를 통해서 데이터에 접근. yellow
+
+rainbowColors.push("ultraviolet"); // 배열의 마지막에 ultarviolet 추가
+console.log(rainbowColors); // ultraviolet이 추가된 rainbowColors 출력
+
+rainbowColors.pop(); // 배열의 마지막 요소 ultraviolet을 제거
+console.log(rainbowColors); // ultraviolet이 제거된 rainbowColors 출력
+
+for (let i = 0; i < rainbowColors.length; i++) {
+  console.log(rainbowColors[i]);
+}
+
+for (const color of rainbowColors) {
+  console.log(color);
+}
+
+console.log(rainbowColors.length); // 3을 출력
+console.log(rainbowColors[rainbowColors.length - 1]); // length 를 응용하여 배열의 마지막 요소도 쉽게 찾아서 출력 가능!
