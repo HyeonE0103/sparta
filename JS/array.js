0fa3f4aad69f0143aa1a34c669a4a7ff7cbfbb6b
HyeonE <mag30102000@gmail.com>
2023-04-27 21:57:30 +0900

    배열

--- JS/array.js
+++ JS/array.js
@@ -25,3 +25,11 @@ for (const color of rainbowColors) {
 
 console.log(rainbowColors.length); // 3을 출력
 console.log(rainbowColors[rainbowColors.length - 1]); // length 를 응용하여 배열의 마지막 요소도 쉽게 찾아서 출력 가능!
+
+const items = [10, 20, 30, 40, 50];
+let total = 0;
+for (const item of items) {
+  total += item;
+}
+let avg = total / items.length;
+console.log(`합계: ${total}, 평균: ${avg}`);
