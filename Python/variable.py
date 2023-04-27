a1167874087f0e082b7b21736f38c92375961c55
HyeonE <mag30102000@gmail.com>
2023-04-28 00:14:02 +0900

    Bool자료형

--- Python/variable.py
+++ Python/variable.py
@@ -1,9 +1,9 @@
+#숫자형 자료형
 a = 5
 b = 4.8
 
 a = 7
 b = 2
-
 a+b   # 9 
 a-b   # 5
 a*b   # 14
@@ -21,3 +21,23 @@ a += 3    # 줄여 쓸 수도 있음 같은 의미!
 a//b  # 3 (몫)
 a%b   # 1 (나머지)
 a**b  # 49 (거듭제곱)
+
+#Bool 자료형
+x = True   # 참
+y = False  # 거짓
+
+# 소문자로 쓰면 자료형으로 인식하지 않고 변수명이라 생각해 에러가 남
+# z = true   # name 'true' is not defined
+# True = 1   # True/False는 변수명으로 쓸 수 없음
+
+4 > 2      # True  크다
+5 < 1      # False 작다
+6 >= 5     # True  크거나 같다
+4 <= 4     # True  작거나 같다
+3 == 5     # False 같다
+4 != 7     # True  같지 않다
+
+a = 4 > 2  # True
+not a      # False    NOT 연산자로 참을 거짓으로, 거짓을 참으로 바꿔줌
+a and b    # False    AND 연산자로 모두 참이어야 참을 반환
+a or b     # True     OR 연산자로 둘 중 하나만 참이면 참
