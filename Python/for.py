f343e8e34deac9a2ec906d451614ebbe8880a5c4
HyeonE <mag30102000@gmail.com>
2023-05-05 23:46:55 +0900

    반복문퀴즈

--- Python/for.py
+++ Python/for.py
@@ -26,3 +26,23 @@ for i, fruit in enumerate(fruits):
     print(i, fruit)
     if i == 2:
         break
+
+num_list = [1, 2, 3, 6, 3, 2, 4, 5, 6, 2, 4]
+
+even_num = 0
+total = 0
+max_num = 0
+for num in num_list:
+    if num % 2 == 0:
+        even_num += 1
+    total += num
+    if max_num < num:
+        max_num = num
+
+print("짝수의 개수:", even_num)
+print("리스트 모든 수의 합:", total)
+print("가장 큰 수:", max_num)
+print("가장 큰 수:", (max(num_list)))
+
+
+
