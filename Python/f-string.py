f83d9b6c5a8b0b3d300212445ba51acdb9433404
HyeonE <mag30102000@gmail.com>
2023-05-09 22:42:00 +0900

    f-string

---
+++ Python/f-string.py
@@ -0,0 +1,24 @@
+scores = [
+    {'name':'영수','score':70},
+    {'name':'영희','score':65},
+    {'name':'기찬','score':75},
+    {'name':'희수','score':23},
+    {'name':'서경','score':99},
+    {'name':'미주','score':100},
+    {'name':'병태','score':32}    
+]
+
+for s in scores:
+  name = s['name']
+  score = str(s['score'])
+  print(name,score)
+
+for s in scores:
+    name = s['name']
+    score = str(s['score'])
+    print(name+'는 '+score+'점 입니다')
+
+for s in scores:
+    name = s['name']
+    score = str(s['score'])
+    print(f'{name}은 {score}점입니다')
